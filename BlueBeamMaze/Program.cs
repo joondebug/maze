19816bfe1f7de8941ae0ceed25f8d75d25ae27dd
unknown <KevinL@IS3.NTHOME.NDRCT.com>
2016-11-22 16:50:03 -0800

    first commit

---
+++ BlueBeamMaze/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace BlueBeamMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap bitMap = new Bitmap("maze1.png");
+            Color pixelColor = bitMap.GetPixel(0, 1);
+
+            bitMap.SetPixel(0, 1, Color.White);
+
+            bitMap.Save("C:/Users/kevinl/Documents/_projects/BlueBeam/Output/maze1.png");
+
+        }
+    }
+}
